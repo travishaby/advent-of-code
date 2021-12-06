af06dc218742bd5a23425d55cd90464a81737260
Travis Haby <travishaby@gmail.com>
2021-12-06 16:21:28 -0700

    Naive first pass at the first part of day3

--- 2021/day3.ts
+++ 2021/day3.ts
@@ -4,8 +4,36 @@ import * as path from 'path';
 const fileName = path.parse(__filename).name
 const puzzleInput = getInput(fileName)
 
-const partOne = puzzleInput
+// iterates over a list of binary numbers
+// and determines the most common bit, 1 or 0, for the corresponding index
+function extractBitExtremity(input: string[], index: number, extreme: 'min' | 'max'): string {
+    const bitCounts: { [key: string]: number } = {
+        '0': 0,
+        '1': 0
+    };
 
-console.log('Part 1 Answer: ', partOne);
+    for (const inputString of input) {
+        const bit = inputString[index];
+        bitCounts[bit]++;
+    }
 
+    let result: string;
+    if (extreme === 'min') {
+        const min = Math.min(...Object.values(bitCounts));
+        result = min === bitCounts['0'] ? '0' : '1';
+    } else {
+        const max = Math.max(...Object.values(bitCounts));
+        result = max === bitCounts['0'] ? '0' : '1';
+    }
+    return result
+}
 
+const mostCommonBits = puzzleInput[0].split('').map((_, index) => extractBitExtremity(puzzleInput, index, 'max'));
+const gammaRate = parseInt(mostCommonBits.join(''), 2);
+
+const leastCommonBits = puzzleInput[0].split('').map((_, index) => extractBitExtremity(puzzleInput, index, 'min'));
+const epsilonRate = parseInt(leastCommonBits.join(''), 2);
+
+console.log('Part 1 gammeRate: ', gammaRate);
+console.log('Part 1 episilonRate: ', epsilonRate);
+console.log('Part 1 Answer: ', gammaRate * epsilonRate);
