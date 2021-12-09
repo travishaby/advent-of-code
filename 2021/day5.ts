9f6c12f2a1f0f3726053bb9f60561d692f5b85d0
Travis Haby <travishaby@gmail.com>
2021-12-09 08:48:41 -0700

    Parse into number pairs

--- 2021/day5.ts
+++ 2021/day5.ts
@@ -2,5 +2,10 @@ import { getInput } from './get_input';
 
 const puzzleInput = getInput(__filename)
 
-console.log(puzzleInput)
+type Point = [number, number];
 
+const deltas = puzzleInput.map(line => {
+    return line.split(' -> ').map(x => x.split(',').map(Number))
+})
+
+console.log(deltas);
