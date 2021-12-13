388e13865d642deaf8a26828e6b471eb658ab5b1
Travis Haby <travishaby@gmail.com>
2021-12-13 09:55:05 -0700

    day 6 part 1 is fine but part 2 causes the node process to run out of memory 😬

--- 2021/day6.ts
+++ 2021/day6.ts
@@ -4,4 +4,35 @@ const puzzleInput = getInput(__filename)
 
 const initialState = puzzleInput[0].split(',').map(Number)
 
-console.log(initialState)
+// part 1
+
+// Each day, a 0 becomes a 6 and adds a new 8 to the end of the list,
+// while each other number decreases by 1 if it was present at the start of the day.
+function tickDay(fish: number[]) {
+    const newFish: number[] = []
+    const tickedFish = fish.reduce((acc, curr) => {
+        if (curr === 0) {
+            acc.push(6)
+            newFish.push(8)
+        } else {
+            acc.push(curr - 1)
+        }
+        return acc
+    }, [] as number[])
+    return [...tickedFish, ...newFish]
+}
+
+const result = tickDay(initialState)
+
+let current = [...initialState];
+for (let i = 0; i < 80; i++) {
+    current = tickDay(current)
+}
+console.log('Part 1: ', current.length);
+
+// part 2
+let part2 = [...initialState];
+for (let i = 0; i < 256; i++) {
+    part2 = tickDay(part2)
+}
+console.log('Part 2: ', part2.length);
